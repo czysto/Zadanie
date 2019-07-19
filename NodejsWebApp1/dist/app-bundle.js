502000557b03d05c8a80de579702a8d7b0d96863
Lukasz <lukww@wp.pl>
2019-07-19 21:30:20 +0200

    next

--- NodejsWebApp1/dist/app-bundle.js
+++ NodejsWebApp1/dist/app-bundle.js
@@ -117,7 +117,7 @@ var Hello = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Hello.prototype.render = function () {
-        return (React.createElement("h1", null, "Welcome to React!!"));
+        return (React.createElement("h1", null, "Lista dokumentów"));
     };
     return Hello;
 }(React.Component));
